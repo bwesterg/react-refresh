5cd76e8ba036f7e1d88f36a97ec2c65bceae67d4
bwesterg <benton.westergaard@gmail.com>
2023-08-09 14:38:31 -0600

    finishes basic backend with index method, adds cors to begin testing frontend.

--- basic_backend/db/seeds.rb
+++ basic_backend/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Trail.create(name: "Sella Ronda", description: "Scenic loop in the Dolomites", distance: 60.0, start_altitude: 1000.0, finish_altitude: 1000.0, elevation_gain: 3000.0)
+Trail.create(name: "Commando Run", description: "Historic hike outside Vail", distance: 16.0, start_altitude: 2800.0, finish_altitude: 2200.0, elevation_gain: 2000.0)
+Trail.create(name: "Betasso Preserve", description: "Easy hike outside Boulder", distance: 8.0, start_altitude: 2000.0, finish_altitude: 2000.0, elevation_gain: 325.0)
